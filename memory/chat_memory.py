3754fa51c80b59239c0bd8308c71e527b8631358
aravind2225 <aravindudiyana123@gmail.com>
2026-05-14 15:10:11 +0530

    Created Chat memory file for story chat history.

---
+++ memory/chat_memory.py
@@ -0,0 +1,12 @@
+class ChatMemory:
+    def __init__(self):
+        self.messages = []
+
+    def add(self, role, content):
+        self.messages.append({
+            "role": role,
+            "content": content
+        })
+
+    def get(self):
+        return self.messages
